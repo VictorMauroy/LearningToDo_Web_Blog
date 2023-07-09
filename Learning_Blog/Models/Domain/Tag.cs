45d226ffeffc526585b60c35c7e593b2d90209d1
Victor Mauroy <mauroy.victor@gmail.com>
2023-07-09 17:39:43 +0200

    feat: :sparkles: create two models => post and tag

---
+++ Learning_Blog/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Learning_Blog.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string DisplayName { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
